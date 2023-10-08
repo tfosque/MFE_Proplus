10009f50176b09d02b5de715265f92ed7bf89997
tim.fosque <tim.fosque@becn.com>
2023-10-08 11:18:04 -0500

    wip orders

--- orders/src/LineItem.jsx
+++ orders/src/LineItem.jsx
@@ -1,43 +1,45 @@
-
-
-export default function LineItem ( props ) {
-  console.log( { props } );
+export default function LineItem(props) {
+  console.log({ props });
   const order = props.order;
   const orderId = order.orderId;
-  const address = order.shipping.address.address2 || '';
+  const address = order.shipping.address.address2 || "";
   const purchaseOrderNo = order.purchaseOrderNo;
   const job = order.job.jobName;
-  const orderDate = order.orderPlaceDate;
+  const orderPlacedDate = order.orderPlacedDate;
   const proplusOrder = order.source;
-  const orderStatus = order.orderStatus;
+  const orderStatusValue = order.orderStatusValue;
   const total = order.total || order.totalMsg;
+  console.group("orderPlaceDate");
+  console.log(order.orderPlacedDate);
+  console.groupEnd();
 
   return (
     <li className="list-group-item">
       <div className="row">
         <div className="col-auto">
-          <input className="form-check-input me-1" type="checkbox" value="" id="firstCheckbox" />
+          <input
+            className="form-check-input me-1"
+            type="checkbox"
+            value=""
+            id="firstCheckbox"
+          />
         </div>
-        <div className="col-auto">
-          {orderId}
-        </div>
-        <div className="col-5">
+        <div className="col-1">{orderId}</div>
+        <div className="col-4">
           <div>{address}</div>
           <div>Cust PO: {purchaseOrderNo}</div>
           <div>Job Account: {job}</div>
-          <div>Date: {orderDate}</div>
+          <div>Date: {orderPlacedDate}</div>
         </div>
-        <div className="col-auto">
-          {proplusOrder}
+        <div className="col-1">{proplusOrder}</div>
+        <div className="col-4">
+          <span>
+            {typeof total === "string" ? "" : "$"}
+            {`${total}`}
+          </span>
         </div>
-        <div className="col-3">
-          <span>{`total? '$': ''`}</span> {total}
-        </div>
-        <div className="col-1">
-          {orderStatus}
-        </div>
+        <div className="col-1">{orderStatusValue}</div>
       </div>
     </li>
-  )
+  );
 }
-
--- orders/src/Orders.jsx
+++ orders/src/Orders.jsx
@@ -1,45 +1,45 @@
 import LineItem from "./LineItem";
 import { data } from "./data";
 
-
 export default () => {
   const orders = data.orders;
-  console.log( { orders } );
+  console.log({ orders });
 
-  const items = orders.map( ( order ) => {
-    return (
-      <LineItem order={order} />
-    )
-  } )
+  const items = orders.map((order) => {
+    return <LineItem order={order} />;
+  });
 
   return (
     <div className="container-fluid mt-2">
       <div className="row">
         <div className="col-12">
           <h2>Your Orders</h2>
-          <div>Get the latest delivery status from your recent orders or find a past order.</div>
+          <div>
+            Get the latest delivery status from your recent orders or find a
+            past order.
+          </div>
         </div>
 
         <div className="col-12 mt-2">
-          <ul className="list-group list-group-flush">
+          <ul
+            className="list-group list-group-flush"
+            style={{ fontSize: ".875rem" }}
+          >
             <li className="list-group-item">
               <div className="row">
                 <div className="col-auto">
-                  <input className="form-check-input me-1" type="checkbox" value="" id="firstCheckbox" />
+                  <input
+                    className="form-check-input me-1"
+                    type="checkbox"
+                    value=""
+                    id="firstCheckbox"
+                  />
                 </div>
-                <div className="col-auto">
-                  Order#
-                </div>
-                <div className="col-5">
-                  Details
-                </div>
-                <div className="col-auto">
-                  Pro+
-                </div>
-                <div className="col-3">Total</div>
-                <div className="col-1">
-                  Status
-                </div>
+                <div className="col-1">Order#</div>
+                <div className="col-4">Details</div>
+                <div className="col-1">Pro+</div>
+                <div className="col-4">Total</div>
+                <div className="col-1">Status</div>
               </div>
             </li>
 
@@ -48,5 +48,5 @@ export default () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
